6ca5951bcf22bf29ecb97a1f12fbfbd00f2c7beb
sharon <sharon.wu@ossii.com.tw>
2021-11-04 17:45:06 +0800

    navbar icon 增加顯示未讀數量

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -36,6 +36,7 @@ use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
 use OCP\Comments\CommentsEntityEvent;
 use OCP\Util;
+use OCP\EventDispatcher\IEventDispatcher;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'announcementcenter';
@@ -54,6 +55,10 @@ class Application extends App implements IBootstrap {
 			$context->registerEventListener(CommentsEntityEvent::EVENT_ENTITY, CommentsEntityListener::class);
 		}
 		$context->registerNotifierService(Notifier::class);
+		$dispatcher = $this->getContainer()->query(IEventDispatcher::class);
+		$dispatcher->addListener('OCA\Files::loadAdditionalScripts', function() {
+			\OCP\Util::addScript(self::APP_ID, 'newannounce');
+		});
 	}
 
 	public function boot(IBootContext $context): void {
