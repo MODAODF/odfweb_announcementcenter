729ce2739c70a34739957533ab4ba2bef2c57e11
tony <tony.yang@ossii.com.tw>
2023-12-05 14:26:50 +0800

    1. 修正原始碼於 js 目錄下的錯誤 2. 修正升級至 nc26 的類別、標籤、屬性命名更新

--- webpack.js
+++ webpack.js
@@ -3,5 +3,6 @@ const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 webpackConfig.entry.admin = path.join(__dirname, 'src', 'admin')
 webpackConfig.entry.dashboard = path.join(__dirname, 'src', 'dashboard')
+webpackConfig.entry.newannounce = path.join(__dirname, 'src', 'newannounce.js')
 
 module.exports = webpackConfig
